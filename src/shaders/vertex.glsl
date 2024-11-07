bc84a88dc9845615706f818ee3185ca3aebaf5f2
Manqoba <manqobazwane22@gmail.com>
2024-11-07 19:35:17 +0200

    feat: include simplex 3D noise algorithm in vertex shader for dynamic displacement

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,7 +1,12 @@
+#include ./includes/simplex3Dnoise.glsl;
+#include <packing>
+
 varying vec2 vUv;
+uniform float time;
 
 void main() {
   vUv = uv;
-
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vec3 pos = position;
+  pos.y += 0.01*snoise(vec3(vUv*30.0, time));
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
