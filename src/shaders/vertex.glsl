e03d70c106534ed22a25645e2fe256c585c694ac
Manqoba <manqobazwane22@gmail.com>
2024-11-07 20:57:49 +0200

    feat(shader): utilise y attribute for vertical offset in vertex displacement

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -3,10 +3,12 @@
 
 varying vec2 vUv;
 uniform float time;
+attribute float y;
 
 void main() {
   vUv = uv;
+  vec2 vuv1 = vec2(vUv.x, y);
   vec3 pos = position;
-  pos.y += 0.01*snoise(vec3(vUv*30.0, time));
+  pos.y += 0.02*snoise(vec3(vuv1*30.0, time));
   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
