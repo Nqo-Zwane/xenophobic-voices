6fcc7a45440e92f9a84de5f759af96082610b5db
Manqoba <manqobazwane22@gmail.com>
2024-11-18 11:59:40 +0200

    feat: link grid line color to depth information using vDepth in fragment shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,5 +1,9 @@
-varying vec2 vUv;
+#include <packing>
+varying float vDepth;
 
 void main() {
-  gl_FragColor = vec4(0.0, 0.0, 0.24, 1.0);
+  float tomix = smoothstep(0.2, 1.0, vDepth);
+  gl_FragColor.rgb = mix(vec3(0.495, 0.165, 0.234), vec3(0.000, 0.001, 0.242), tomix);
+
+	gl_FragColor.a = 1.0;
 }
